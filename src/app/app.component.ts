8f1ee7c9e870893bd6aa674d0d90926e660d75e3
cordobarafaela <cordobarafaela@gmail.com>
2022-04-05 09:36:59 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '01-bases';
+
+
+/*
+sumar() {
+this.numero += 1 ;
+}
+
+restar() {
+  this.numero -= 1 ;
+  }
+*/
+
+
+
+
 }
